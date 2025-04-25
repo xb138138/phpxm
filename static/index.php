deee8a4b96ac2fbf2942464db6336497ea30c3fe
xb <1439545090@qq.com>
2025-04-25 22:59:21 +0800

    1

--- static/index.php
+++ static/index.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>太阳论坛</title>
-    <link rel="stylesheet" href="../style/index.css">
+    <link rel="stylesheet" href="../style/style.css">
     <link rel="stylesheet" href="../style/media.css">
 </head>
 <body>
@@ -39,30 +39,31 @@
                 </div>
             </div>
             <div class="waterfall">
-                <div class="card">
+                <a href="detail.php" class="card" style="text-decoration:none; color:inherit;">
                     <h5>【公告】论坛新功能上线</h5>
                     <p>我们上线了全新的瀑布流首页，欢迎大家体验！</p>
-                </div>
-                <div class="card">
+                </a>
+                <a href="detail.php" class="card" style="text-decoration:none; color:inherit;">
                     <h5>【分享】我的学习心得</h5>
                     <p>最近在学习前端开发，收获很大，欢迎交流！</p>
-                </div>
-                <div class="card">
+                </a>
+                <a href="detail.php" class="card" style="text-decoration:none; color:inherit;">
                     <h5>【提问】如何高效背单词？</h5>
                     <p>有没有背单词的好方法推荐？</p>
-                </div>
-                <div class="card">
+                </a>
+                <a href="detail.php" class="card" style="text-decoration:none; color:inherit;">
                     <h5>【经验】时间管理技巧</h5>
                     <p>合理规划时间，让生活更高效！</p>
-                </div>
-                <div class="card">
+                </a>
+                <a href="detail.php" class="card" style="text-decoration:none; color:inherit;">
                     <h5>【讨论】你喜欢早起吗？</h5>
                     <p>早起真的有用吗？大家怎么看？</p>
-                </div>
-                <div class="card">
+                </a>
+                <a href="detail.php" class="card" style="text-decoration:none; color:inherit;">
                     <h5>【公告】论坛维护通知</h5>
                     <p>本周末论坛将进行维护，敬请谅解。</p>
-                </div>
+                </a>
+
             </div>
         </div>
     </div>
